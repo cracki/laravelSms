bc130b5cbac2fbf363aea2c2682b65622712a2f4
kamel <cracki@gmail.com>
2015-10-17 16:48:36 +0330

    add support to env variable

--- src/config/config.php
+++ src/config/config.php
@@ -2,10 +2,10 @@
 
 return array (
 
-	'webserviceUrl' => 'http://api.payamak-panel.com/post/Send.asmx?wsdl',
-	'username'		=> '9123456789',
-	'password'		=> '****',
-	'from'			=> '****',
+	'webserviceUrl' => env('SMS_WEBSERVICE', 'http://api.payamak-panel.com/post/Send.asmx?wsdl'),
+	'username'		=> env('SMS_USERNAME', '9123456789'),
+	'password'		=> env('SMS_PASSWORD','****'),
+	'from'			=> env('SMS_FROM','****'),
 	'isflash'		=> false,
 	'udh'			=> '',
 	'recId'			=> array(0),
